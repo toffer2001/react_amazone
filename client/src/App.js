5d7b89fbc741225af293477d6bb10b6896ac6603
Kristoffer Keene <toffer2001@yahoo.com>
2019-07-20 13:21:56 -0600

    .

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,26 @@
 import React, { Fragment, } from 'react';
-import Home from './components/Home/landingPage';
+import Home from './components/Home/Home';
 import { Switch, Route, } from 'react-router-dom';
 import { Container, } from "semantic-ui-react";
+import FetchUser from './components/Auth/FetchUser';
+import ProtectedRoute from './components/Auth/ProtectedRoute';
+import Login from './components/Home/Login';
+import Register from './components/Home/Register';
+import NoMatch from './components/Home/NoMatch';
 
 const App = () => (
   <Fragment>
+    <FetchUser>
     <Container>
       <Switch>
-        <Route exact path="/" component={Home} />
+        <ProtectedRoute exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+        <Route component={NoMatch} />
       </Switch>
       </Container>
+      </FetchUser>
+
   </Fragment>
 )
 
