1f77f80175554150482c10afc7d1cc06723aa5e9
Logan Bryant <loganbryantvertical@gmail.com>
2019-07-20 12:35:34 -0600

    reinitializing logan branch

--- db/migrate/20190720160412_create_products.rb
+++ db/migrate/20190720160412_create_products.rb
@@ -2,7 +2,7 @@ class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
       t.string :name
-      t.string :desription
+      t.string :description
       t.float :price
       t.integer :stock
       t.belongs_to :department, foreign_key: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,60 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+require 'database_cleaner'
+
+DatabaseCleaner.clean_with(:truncation)
+
+user = User.new({
+  name: 'user',
+  email: 'user@email.com',
+  password: 'password',
+})
+user.save!
+
+
+department = Department.new({
+    title: 'Beer',
+})
+department.save!
+
+7.times do 
+  product = Product.create(
+      name: Faker::Beer.brand,
+      description:Faker::Beer.style,
+      price: Faker::Number.between(3, 11),
+      stock: Faker::Number.between(4, 20),
+      department_id: 1
+  )
+  end
+
+department = Department.new({
+    title: 'Coffee',
+})
+department.save!
+
+14.times do 
+  product = Product.create(
+      name: Faker::Coffee.blend_name,
+      description:Faker::Coffee.notes,
+      price: Faker::Number.between(5, 30),
+      stock: Faker::Number.between(12, 55),
+      department_id: 2
+  )
+  end
+
+department = Department.new({
+    title: 'Cannabis',
+})
+department.save!
+
+10.times do 
+  product = Product.create(
+      name: Faker::Cannabis.strain,
+      description:Faker::Cannabis.health_benefit,
+      price: Faker::Number.between(5, 30),
+      stock: Faker::Number.between(12, 55),
+      department_id: 3
+  )
+  end
+
+puts "Data Seeded."
+
