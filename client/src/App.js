40de3a46ff9bea6bd88126251f814dcdf6f07653
Zach Woolboy <zachtwoolboy@gmail.com>
2019-07-20 12:23:06 -0600

    finished user auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, } from 'react';
+import Home from './components/Home/Home';
+import { Switch, Route, } from 'react-router-dom';
+import { Container, } from "semantic-ui-react";
+import FetchUser from './components/Auth/FetchUser';
+import ProtectedRoute from './components/Auth/ProtectedRoute';
+import Login from './components/Home/Login';
+import Register from './components/Home/Register';
+import NoMatch from './components/Home/NoMatch';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <Fragment>
+    <FetchUser>
+    <Container>
+      <Switch>
+        <ProtectedRoute exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+        <Route component={NoMatch} />
+      </Switch>
+      </Container>
+      </FetchUser>
+  </Fragment>
+)
 
 export default App;
